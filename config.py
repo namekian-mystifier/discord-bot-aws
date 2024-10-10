0ffe11a9bdda1532304a09bdd1e23cfa443ff31f
namekian-mystifier <namekian@mystifier.xyz>
2024-10-10 16:50:47 +0300

    embarassing mistake, career-ending mistake, CLM

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ LLM_MODEL = None
 DISCORD_MESSAGE_LIMIT = 2000
 INPUT_MESSAGE_LIMIT = None
 #SUMMARIZE_QUERY = "Hello! Can you summarize the discord chat between {[%s]} using bullet points for the main topics of conversation and in less than 1900 characters and get straight to the summary with no intro and don't add any information that isn't present in the discord chat?"
-SUMMARIZE_QUERY = "Hello! As a reputable journalist, can you summarize the discord chat between {[%s]} in max 1900 characters and using bullet points for the main topics and get straight to the summary with no intro and do NOT add any false facts about the content of the conversation in the summary?
+SUMMARIZE_QUERY = "Hello! As a reputable journalist, can you summarize the discord chat between {[%s]} in max 1900 characters and using bullet points for the main topics and get straight to the summary with no intro and do NOT add any false facts about the content of the conversation in the summary?"
 MINIMUM_WAIT_TIME_BETWEEN_SUMMARIES = 1800
 LAST_ACCESSED = None
